afa156a5402f21299e0a7c33eeded3006de412cf
Philippe Morissette <morissette.philippe@gmail.com>
2013-09-26 20:14:32 -0700

    major refactor

--- index.js
+++ index.js
@@ -1,52 +1,20 @@
-var request = require('request'),
-  zlib = require('zlib'),
-  fs = require('fs');
+var torget = require('./torget');
 
-var headers = {
-    "accept-charset" : "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
-    "accept-language" : "en-US,en;q=0.8",
-    "accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "accept-encoding" : "gzip,deflate",
-}
+// default - auto search, select and downlaod
+module.exports = torget.get;
 
-var options = {
-    headers: headers
-}
+// search for torrents and return a list of results
+// based on kickass.to
+module.exports.search = torget.search;
 
-module.exports = function(hash, filename, callback) {
-    // setup options
-    hash = hash.toUpperCase();
-    options.url = "http://torcache.net/torrent/{0}.torrent".replace('{0}', hash);
+// select based on selection algo (for now first in list)
+module.exports.select = torget.select;
 
-    // create request and handle response
-    var req = request(options)
+// select torrent interactively
+module.exports.interactive = torget.interactive;
 
-    req.on('response', function (res) {
-        if (res.statusCode !== 200) {
-            return callback(new Error('Status not 200'));
-        }
-
-        // create writeStream
-        var outStream = null;
-        try {
-            outStream = fs.createWriteStream(filename);
-        } catch(err) {
-            return callback(err);
-        }
-
-        var encoding = res.headers['content-encoding'];
-        if (encoding == 'gzip') {
-            res.pipe(zlib.createGunzip()).pipe(outStream)
-        } else if (encoding == 'deflate') {
-            res.pipe(zlib.createInflate()).pipe(outStream)
-        } else {
-            res.pipe(outStream)
-        }
-
-        return callback(null);
-    });
-
-    req.on('error', function(err) {
-        return callback(err);
-    });
-}
+// download a torrent - object must have property
+// hash and title to be considered ok
+// usually this is used with result from
+// select or autoSelect
+module.exports.download = torget.download;
